d777c44945d6bafdb88a8aa301842e5685d98944
Artem Plotnikov <115026354+printnewcode@users.noreply.github.com>
2026-02-10 18:18:48 +0300

    Update run_cmd.sh

--- run_cmd.sh
+++ run_cmd.sh
@@ -3,4 +3,6 @@
 ssh localhost -p222
 cd ~/omnichannel-crm/omnichannel-crm/
 source venv/bin/activate
+ln -sf /home/r/rusla9m5/.local/lib/libffi.so.6.0.4 /home/r/rusla9m5/.local/lib/libffi.so.6
+export LD_LIBRARY_PATH=/home/r/rusla9m5/.local/lib:$LD_LIBRARY_PATH
 python3 manage.py $@
